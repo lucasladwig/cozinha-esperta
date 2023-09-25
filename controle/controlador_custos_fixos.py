8dc63789f2ec871399877279ad0301df7f8d2e7d
Lucas Ladwig <lucasladwig@gmail.com>
2023-09-25 13:13:51 -0300

    modulo de custos finalizado

--- controle/controlador_custos_fixos.py
+++ controle/controlador_custos_fixos.py
@@ -4,7 +4,7 @@ from persistencia.custos_fixos_dao import CustosFixosDAO
 
 
 class ControladorCustosFixos():
-    """Controlador de custos fixos."""    
+    """Controlador de custos fixos."""
 
     def __init__(self) -> None:
         self.__tela = TelaCustosFixos()
@@ -15,7 +15,7 @@ class ControladorCustosFixos():
         if len(self.dao.cache) == 0:
             custos_iniciais = CustosFixos(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1)
             self.dao.add(custos_iniciais)
-        
+
     # GETTERS/SETTERS
     @property
     def tela(self):
@@ -24,33 +24,46 @@ class ControladorCustosFixos():
     @property
     def dao(self):
         return self.__dao
-    
+
     @property
     def controlador_sistema(self):
         return self.__controlador_sistema
 
     # TELAS
-    def abrir_tela(self):
-        custos_atual = self.dao.get_last()
-        
-        while True:            
-            self.tela.inicializar_tela(custos_atual)
+    def abrir_tela(self) -> None:
+        while True:
+            try:
+                custos_atuais = self.dao.get_last()
+                botao, novos_custos = self.tela.open(custos_atuais)
+                if botao == "Atualizar e Salvar":
+                    custos_atualizados = self.atualizar_custos_fixos(novos_custos)
+                    self.tela.close()
+                    self.tela.open(custos_atualizados)
+                elif botao == "Sair":
+                    break
+                    # self.tela.close()
 
-    #CRUD
-    def atualizar_custos_fixos(self) -> None:
-        self.abrir_tela()
+                self.tela.close()
+                # self.tela.init_components()
+            except ValueError:
+                self.tela.mostrar_mensagem("Por favor, insira valores numéricos para os custos!",
+                                           titulo="Erro ao atualizar custos")
 
-        if not novos_custos:
+    # CRUD
+    def atualizar_custos_fixos(self, novos_custos: dict) -> CustosFixos:
+        if novos_custos is None:
             raise ValueError
+
+        custos_atual = self.dao.get_last()
 
         custos_atual.agua = novos_custos["agua"]
         custos_atual.aluguel = novos_custos["aluguel"]
         custos_atual.eletricidade = novos_custos["eletricidade"]
         custos_atual.gas = novos_custos["gas"]
         custos_atual.manutencao = novos_custos["manutencao"]
-        custos_atual.outro = novos_custos["outros"]
+        custos_atual.outros = novos_custos["outros"]
+        custos_atual.porcoes_produzidas_mes = novos_custos["producao_mensal"]
 
-        self.__dao.add(custos_atual)
+        self.dao.add(custos_atual)
 
-    def voltar(self):
-        self.__controlador_sistema.abrir_tela()
+        return custos_atual
--- limite/tela_custos_fixos.py
+++ limite/tela_custos_fixos.py
@@ -14,7 +14,7 @@ class TelaCustosFixos:
         self.__window = None
 
     # PEGAR DADOS
-    def init_components(self, custos_atual: CustosFixos) -> dict:
+    def init_components(self, custos_atuais: CustosFixos) -> dict:
         """"Gera o layout da janela do gerenciador de custos fixos."""
 
         # Campos de inserção
@@ -22,108 +22,101 @@ class TelaCustosFixos:
                 sg.Text("Água:"),
                 sg.Input(key="agua",
                          size=16,
-                         default_text=f"{custos_atual.agua:.2f}",
+                         default_text=f"{custos_atuais.agua:.2f}",
                          tooltip="Valor da conta de água.")]
         aluguel = [sg.Push(),
                    sg.Text("Aluguel:"),
                    sg.Input(key="aluguel",
                             size=16,
-                            default_text=f"{custos_atual.aluguel:.2f}",
+                            default_text=f"{custos_atuais.aluguel:.2f}",
                             tooltip="Valor do aluguel de espaço de cozinha, caso haja.")]
         eletricidade = [sg.Push(),
                         sg.Text("Eletricidade:"),
                         sg.Input(key="eletricidade",
                                  size=16,
-                                 default_text=f"{custos_atual.eletricidade:.2f}",
+                                 default_text=f"{custos_atuais.eletricidade:.2f}",
                                  tooltip="Valor da conta de eletricidade.")]
         gas = [sg.Push(),
                sg.Text("Gás:"),
                sg.Input(key="gas",
                         size=16,
-                        default_text=f"{custos_atual.gas:.2f}",
+                        default_text=f"{custos_atuais.gas:.2f}",
                         tooltip="Custo com gás de cozinha.")]
         manutencao = [sg.Push(),
                       sg.Text("Manutenção:"),
                       sg.Input(key="manutencao",
                                size=16,
-                               default_text=f"{custos_atual.manutencao:.2f}",
+                               default_text=f"{custos_atuais.manutencao:.2f}",
                                tooltip="Custo com manutenção de equipamentos.")]
         outros = [sg.Push(),
                   sg.Text("Outros:"),
                   sg.Input(key="outros",
                            size=16,
-                           default_text=f"{custos_atual.outros:.2f}",
+                           default_text=f"{custos_atuais.outros:.2f}",
                            tooltip="Custo com produtos de limpeza, embalagens, etc...")]
 
         frame_custos = [
             [sg.Column([agua, aluguel, eletricidade]),
              sg.Column([gas, manutencao, outros])],
-            [sg.Push(), sg.Button("Atualizar Custos Fixos")],
-            [sg.Text(f"Custo fixo total: R$"), sg.Text(key="custo_total")],
+            [sg.Text(
+                f"Custo fixo total: R${custos_atuais.custo_fixo_total:.2f}")],
         ]
 
         layout = [
             [sg.Text("Insira uma estimativa do custo mensal (R$) com os itens abaixo:")],
             [sg.Frame("Custos Fixos:", frame_custos)],
             [sg.Text("Produção mensal total (porções):"),
-             sg.Input(key="producao_mensal", size=14, default_text=""),
-             sg.Button("Pegar do último mês")],
-            [sg.Text("Contribuição do custos fixos por porção:"),
-             sg.Text(key="custo_porcao")],
-            [sg.Push(), sg.Button("Cancelar"), sg.Button("Salvar")]
+             sg.Input(key="producao_mensal",
+                      size=14,
+                      default_text=f"{custos_atuais.porcoes_produzidas_mes}",
+                      tooltip="A soma mensal de todas as porções produzidas de todos os pratos."),
+             sg.Button("Pegar do Último Mês", disabled=True)],
+            [sg.Text(
+                f"Contribuição dos custos fixos em cada porção: R${custos_atuais.custo_fixo_por_porcao:.2f}")],
+            [sg.Push(), sg.Button("Sair"), sg.Button("Atualizar e Salvar")]
         ]
-        try:
-            self.__window = sg.Window("Custos Fixos", layout, resizable=True)
 
-            botao, valores = self.__window.Read()
+        self.__window = sg.Window("Custos Fixos", layout, resizable=True)
 
-            agua_novo = float(valores["agua"])
-            aluguel_novo = float(valores["aluguel"])
-            eletricidade_novo = float(valores["eletricidade"])
-            gas_novo = float(valores["gas"])
-            manutencao_novo = float(valores["manutencao"])
-            outros_novo = float(valores["outros"])
-            producao_mensal_novo = int(valores["producao_mensal"])
-
-            lista_custos_novos = [
-                agua_novo, aluguel_novo, eletricidade_novo, gas_novo, manutencao_novo, outros_novo]
-
-            if (all([custo <= 0 for custo in lista_custos_novos])
-                    or producao_mensal_novo < 1):
-                raise ValueError
-
-            self.__window.close()
-
-            return {
-                "agua": agua_novo,
-                "aluguel": aluguel_novo,
-                "eletricidade": eletricidade_novo,
-                "gas": gas_novo,
-                "manutencao": manutencao_novo,
-                "outros": outros_novo
-            }
-
-        except ValueError:
-            self.mostrar_mensagem(
-                "Por favor, insira valores numéricos para os custos!",
-                titulo="Erro ao atualizar custos")
-
-    def inicializar_tela(self, custos_fixos: CustosFixos):
-        self.init_components(custos_fixos)
+    def open(self, custos: CustosFixos):
+        self.init_components(custos)
         botao, valores = self.__window.read()
 
-        # Atualizar custo total na tela            
-        if botao == "Atualizar Custos Fixos":
-            soma = sum(valores.values())
-            self.__window['custo_total'].update(soma)
+        agua_novo = float(valores["agua"])
+        aluguel_novo = float(valores["aluguel"])
+        eletricidade_novo = float(valores["eletricidade"])
+        gas_novo = float(valores["gas"])
+        manutencao_novo = float(valores["manutencao"])
+        outros_novo = float(valores["outros"])
+        producao_mensal_novo = int(valores["producao_mensal"])
 
+        lista_custos_novos = [
+            agua_novo, aluguel_novo, eletricidade_novo, gas_novo, manutencao_novo, outros_novo]
 
-        if botao in ("Salvar", "Calcular"):
-            return valores
-        elif botao == "Cancelar":
-            return None
+        # Valida valores inseridos
+        if (all([custo <= 0 for custo in lista_custos_novos])
+                or producao_mensal_novo < 1):
+            raise ValueError
+
+        novos_valores = {
+            "agua": agua_novo,
+            "aluguel": aluguel_novo,
+            "eletricidade": eletricidade_novo,
+            "gas": gas_novo,
+            "manutencao": manutencao_novo,
+            "outros": outros_novo,
+            "producao_mensal": producao_mensal_novo
+        }
+
+        return botao, novos_valores
+
+    def close(self):
+        self.__window.close()
 
     # MOSTRAR MENSAGENS
-
     def mostrar_mensagem(self, mensagem, titulo=""):
         sg.popup(mensagem, title=titulo)
+
+    # def atualizar_valores_tela(self, custos: CustosFixos):
+    #     soma = sum(custos.values())
+    #     self.__window['custo_total'].update(soma)
