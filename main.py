f5ada0c54a1af553ead840a5a61bb7d4b5735462
Lucas Ladwig <lucasladwig@gmail.com>
2023-10-26 16:49:42 -0300

    atualizando main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from controle.controlador_sistema import ControladorSistema
 # from controle.controlador_estoque_insumo import ControladorEstoqueInsumo
 # from controle.controlador_custos_fixos import ControladorCustosFixos
 
-# if __name__ == "__main__":
-#     ControladorInsumo().abre_tela()
+if __name__ == "__main__":
+    ControladorSistema().abre_tela()
 
 # if __name__ == "__main__":
 #     ControladorEstoqueInsumo().abre_tela()
