e747c7ee43dbc1958df6a07a4a715fa3c14e80f1
Lucas Ladwig <lucasladwig@gmail.com>
2023-12-11 21:03:37 -0300

    etiqueta e producao

--- controle/controlador_producao.py
+++ controle/controlador_producao.py
@@ -5,6 +5,7 @@ from limite.tela_insere_producao import TelaInsereProducao
 from limite.tela_edita_producao import TelaEditaProducao
 from limite.tela_mensagem import TelaMensagem
 from dados.receitas_dict import Receitas
+from persistencia.insumo_dao import InsumoDAO
 
 from datetime import datetime, date
 
@@ -17,6 +18,19 @@ class ControladorProducao:
         self.__tela_insere_producao = TelaInsereProducao()
         self.__tela_edita_producao = TelaEditaProducao()
         self.__tela_mensagem = TelaMensagem()
+        self.__insumo_dao = InsumoDAO()
+
+    @property
+    def controlador_sistema(self):
+        return self.__controlador_sistema
+
+    @property
+    def producao_dao(self):
+        return self.__producao_dao
+
+    @property
+    def insumo_dao(self):
+        return self.__insumo_dao
 
     def edita_producao(self, valores, id):
         # self.__tela_estoque_insumo.close()
@@ -51,10 +65,10 @@ class ControladorProducao:
             except ValueError:
                 return self.__tela_mensagem.open("Erro na Data")
 
-        if data_formatada < datetime.now().date():
+        if data_formatada.date() < datetime.now().date():
             return self.__tela_mensagem.open("A data de produção não pode ser anterior a hoje!")
 
-        if data_formatada != producao.data_producao:
+        if data_formatada.date() != producao.data_producao:
             producao.data_producao = data_formatada.date()
 
         self.__producao_dao.add(producao=producao)
@@ -81,7 +95,7 @@ class ControladorProducao:
             return self.__tela_mensagem.open("Erro na Data")
 
         producao = Producao(receita=potencial_producao["it_receita"],
-                            custo_total_producao=potencial_producao["it_receita"]["custo_porcao"],
+                            custo_total_producao=potencial_producao["it_receita"].custo_porcao,
                             numero_porcoes=quantidade_porcoes,
                             data_producao=data_formatada.date(),
                             status=False)
@@ -100,19 +114,19 @@ class ControladorProducao:
     def lista_dados_producao(self, id: str):
         lista_objeto_producao = []
         producao = self.busca_producao_por_id(id)
-        lista_objeto_producao.append(producao.receita["nome"].title())
+        lista_objeto_producao.append(producao.receita.nome.title())
         lista_objeto_producao.append(producao.numero_porcoes)
         lista_objeto_producao.append(producao.data_producao)
         return lista_objeto_producao
 
     def __monta_lista(self):
         lista_producao = []
-        producoes = self.__producao_dao.get_all()
+        producoes = self.producao_dao.get_all()
         try:
             for values in producoes:
                 lista_auxiliar = []
                 lista_auxiliar.append(values.id)
-                lista_auxiliar.append(values.receita['nome'])
+                lista_auxiliar.append(values.receita.nome)
                 lista_auxiliar.append(values.custo_total_producao)
                 lista_auxiliar.append(datetime.strftime(
                     values.data_producao, "%Y-%m-%d"))
@@ -131,9 +145,23 @@ class ControladorProducao:
                 "Receita Já Produzida")
             return
         producao.status = True
+
+        itensReceita = producao.receita.itens  # aqui buscamos todos os itens da receita
+        for item in itensReceita:
+            quantidade_insumo = (
+                item.qtd_bruta / producao.receita.rendimento_porcoes) * producao.numero_porcoes
+            if quantidade_insumo > item.insumo.estoque_atual:
+                diferenca = quantidade_insumo - item.insumo.estoque_atual
+                item.insumo.estoque_atual = 0
+
+                self.controlador_sistema.controlador_lista_compras.adicionar_na_lista_de_compras(
+                    {"it_nome": item.insumo.nome, "it_quantidade": diferenca})
+
+            else:
+                item.insumo.estoque_atual -= quantidade_insumo
+
         self.__tela_mensagem.open("Alteração Concluida!")
         return producao
-
         # dispara metodo para efetivar os intens utilizados
 
     def abre_tela(self):
@@ -142,7 +170,10 @@ class ControladorProducao:
             self.__tela_producao.close()
 
             if botao == "Nova Produção":
-                receitas = Receitas
+                # receitas = Receitas
+                receitas = list(
+                    self.__controlador_sistema.controlador_receitas.receita_dao.get_all())
+
                 informacoes = self.__tela_insere_producao.open(
                     lista=None, receitas=receitas)
                 if informacoes:
